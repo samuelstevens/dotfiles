e5f37127410b68c21959ee142dfd3dcaefda5883
Samuel Stevens <samuel.robert.stevens@gmail.com>
2026-02-06 12:06:36 -0500

    add xan package

---
+++ ghrel/.config/ghrel/packages/xan.py
@@ -0,0 +1,17 @@
+import subprocess
+import re
+
+pkg = "medialab/xan"
+
+asset = {"linux-x86_64": "xan-x86_64-unknown-linux-musl.tar.gz"}
+binary = {"linux-x86_64": "*"}
+
+
+def ghrel_verify(*, version: str, bin_name: str):
+    result = subprocess.run([bin_name, "--version"], capture_output=True, text=True)
+    assert result.returncode == 0
+
+    stdout = result.stdout.strip()
+    assert stdout
+
+    assert re.search(r"^[\d\.]*$", stdout)
