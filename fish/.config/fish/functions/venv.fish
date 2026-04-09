39dcfe147a02a7d50091a3a86c2944bffe5a8750
Samuel Stevens <samuel.stevens@liquid.ai>
2026-04-09 20:45:56 +0000

    walk dir tree for venv fish func

--- fish/.config/fish/functions/venv.fish
+++ fish/.config/fish/functions/venv.fish
@@ -1,10 +1,26 @@
-function venv --description "Activate ./.venv for the current project"
-    set -l activate_path .venv/bin/activate.fish
+function venv --description "Activate .venv for the current project"
+    set -l dir (pwd)
+    set -l activate_path "$dir/.venv/bin/activate.fish"
 
-    if not test -f $activate_path
-        echo "venv: no $activate_path in "(pwd) >&2
-        return 1
+    # Always allow a .venv in the current directory.
+    if test -f $activate_path
+        source $activate_path
+        return 0
     end
 
-    source $activate_path
+    while true
+        if test -f "$dir/.venv/bin/activate.fish"; and test -e "$dir/.git"
+            source "$dir/.venv/bin/activate.fish"
+            return 0
+        end
+
+        if test "$dir" = /
+            break
+        end
+
+        set dir (path dirname $dir)
+    end
+
+    echo "venv: no local .venv and no parent .venv at a git root from "(pwd) >&2
+    return 1
 end
